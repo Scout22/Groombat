7c1c0b98ff86dc916edc8b18fb0b72f57e9c5bb8
scout22 <yanissamy1@gmail.com>
2016-02-29 17:20:03 +0100

    modified:   projet/.classpath 	modified:   projet/bin/Simulator.class 	modified:   projet/bin/Trashcan.class 	modified:   projet/src/Simulator.java 	modified:   projet/src/Trashcan.java

--- projet/src/Simulator.java
+++ projet/src/Simulator.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
 
 public class Simulator {
-
+	private ArrayList <Obstacle> obstacles;
+	private ArrayList <DirtSpot> dirtSpots;
+	private ArrayList <Robot> robots;
 	public Simulator() {
-		// TODO Auto-generated constructor stub
+		obstacles=new ArrayList <Obstacle>();
+		dirtSpots=new ArrayList <DirtSpot>();
+		robots=new ArrayList <Robot>();
+		}
+	public void addDirtSpot(DirtSpot ds){
+		dirtSpots.add(ds);		
 	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void addObstacle(Obstacle ob){
+		obstacles.add(ob);		
+	}
+	public void addRobot(Robot rob){
+		robots.add(rob);
 	}
 
 }
